da4543ad039fb9228c7347616480d764f9f7b383
Mark Bean <mbean0302@gmail.com>
2022-10-09 21:57:06 -0400

    Alpha 1.1

---
+++ scripts/auth.js
@@ -0,0 +1,96 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    // load guides and UI
+    if (user) {
+        db.collection('guides').onSnapshot(snapshot => {
+                setupGuides(snapshot.docs)
+                setupUI(user)
+            }, err => console.log(err.message))
+    } else {
+        setupUI(null)
+        setupGuides([])
+        
+    }
+})
+
+// create guide
+const createForm = document.querySelector('#create-form')
+
+createForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const guide = {
+      title: createForm.title.value,
+      content: createForm.content.value
+    }
+
+    db.collection('guides').add(guide)
+        .then(() => {
+            // close modal and reset form
+            const modal = document.querySelector('.modal-create')
+            M.Modal.getInstance(modal).close()
+            createForm.reset()
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+})
+
+// signup
+const signupForm = document.querySelector('#signup-form')
+
+signupForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    // get user info
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+
+    // signup the user
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(cred => {
+            return db.collection('users').doc(cred.user.uid).set({
+                bio: signupForm['signup-bio'].value
+            })
+            .then(() => {
+                const modal = document.querySelector('#modal-signup')
+                M.Modal.getInstance(modal).close()
+                signupForm.reset()
+            })
+            
+        })
+})
+
+// log out
+const logout = document.querySelector('#logout')
+
+logout.addEventListener('click', e => {
+    e.preventDefault()
+    // sign user out
+    auth.signOut()
+        .then(() => {
+            console.log('This user has logged out.')
+        })
+})
+
+// log in
+const loginForm = document.querySelector('#login-form')
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault()
+    // get user info
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+    
+    // login the user
+    auth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            console.log('This user has logged in.')
+            const modal = document.querySelector('#modal-login')
+            M.Modal.getInstance(modal).close()
+            loginForm.reset()
+        })
+})
+
+
---
+++ scripts/index.js
@@ -0,0 +1,79 @@
+const guideList = document.querySelector('.guides')
+const loggedOutLinks = document.querySelectorAll('.logged-out')
+const loggedInLinks = document.querySelectorAll('.logged-in')
+const accountDetails = document.querySelector('.account-details')
+
+//setup UI
+const setupUI = user => {
+    if (user) {
+        // toggle UI elements
+        loggedInLinks.forEach(item => item.style.display = 'block')
+        loggedOutLinks.forEach(item => item.style.display = 'none')
+
+        // get user UID
+        db.collection('users').doc(user.uid).get().then(doc => {
+            // show user details
+        const html = `
+        <table class="centered highlight">
+            <tr>
+                <th>Email Address:</th>
+                <td>${user.email}</td>
+            </tr>
+            <tr>
+                <th>Bio:</th>
+                <td style="font-style: italic">${doc.data().bio}</td>
+            </tr>
+        </table>
+        `
+
+        accountDetails.innerHTML += html
+        })
+
+        
+    } else {
+        loggedInLinks.forEach(item => item.style.display = 'none')
+        loggedOutLinks.forEach(item => item.style.display = 'block')
+
+        // remove user details
+        accountDetails.innerHTML = ''
+    }
+}
+
+//setup guides
+const setupGuides = data => {
+    if (data.length) {
+        let html = ''
+
+            data.forEach(doc => {
+                const title = doc.data().title
+                const content = doc.data().content
+                const li = `
+                    <li>
+                        <div class="collapsible-header grey lighten-4">${title}</div>
+                        <div class="collapsible-body white">${content}</div>
+                    </li>
+                `
+                html += li
+            })
+            
+            guideList.innerHTML = html
+
+} else {
+    guideList.innerHTML = `
+        <h5 class="center-align">Login to view guides.</h5>
+    `
+    }
+    
+}
+
+    
+
+// set up materialize components
+document.addEventListener('DOMContentLoaded', () => {
+    const modals = document.querySelectorAll('.modal')
+    M.Modal.init(modals)
+
+    const items = document.querySelectorAll('.collapsible')
+    M.Collapsible.init(items)
+})
+
